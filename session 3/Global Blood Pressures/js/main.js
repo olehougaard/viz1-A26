c9ec725fbbd65d7cfa0b408f2f191cd1002a0997
Ole Hougaard <olehougaard@gmail.com>
2026-09-11 15:08:26 +0200

    Implemented stacked Bar Chart

--- session 3/Global Blood Pressures/js/main.js
+++ session 3/Global Blood Pressures/js/main.js
@@ -25,181 +25,22 @@ function descriptiveStatistics(data, extractor) {
   return {min, max, mean, median, q1, q3}
 }
 
-function visualizeDiastolicSystolic() {
-  d3.select("#container").selectAll("*").remove()
-  const svg = d3.select("#container").append("svg")
-    .attr("viewBox", "0 0 800 600")
-
-  const width = 800
-  const height = 600
-  const margins = {top: 50, right: 50, bottom: 50, left: 50}
-  const visibleWidth = width - margins.left - margins.right
-  const visibleHeight = height - margins.top - margins.bottom
-
-  const visibleArea = svg.append("g")
-    .attr("transform", 
-          `translate(${margins.left}, ${margins.top}) 
-           scale(${visibleWidth / width}, ${visibleHeight / height})`)
-
-  const diastolicScale = d3.scaleLinear()
-    .domain([25, 130])
-    .range([height, 0])
-
-  const systolicScale = d3.scaleLinear()
-    .domain([40, 250])
-    .range([0, width])
-
-  visibleArea.append("g")
-    .attr("id", "horizontal-axis")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(systolicScale))
-    .attr("stroke-width", 2)
-    .selectAll("text")
-      .attr("y", 10)
-
-  visibleArea.append("text")
-  .attr("x", 670)
-  .attr("y", 645)
-  .attr("font-size", 20)
-  .text("Systolic Pressure")
-
-  visibleArea.append("g")
-  .attr("id", "vertical-axis")
-  .call(d3.axisLeft(diastolicScale))
-  .attr("stroke-width", 2)
-  .selectAll("text")
-  .attr("x", -10)
-
-  visibleArea.append("text")
-    .attr("x", 30)
-    .attr("y", 20)
-    .attr("font-size", 20)
-    .text("Diastolic Pressure")
-
-  visibleArea.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("cx", d => systolicScale(d.Systolic_BP_mmHg))
-    .attr("cy", d => diastolicScale(d.Diastolic_BP_mmHg))
-    .attr("r", 1)
-    .attr("fill", "black")
-}
-
-function visualizeBloodPressureByCountry() {
-  const groupedData = d3.group(data, d => d.Country)
-  const bloodPressureByCountry = Array.from(groupedData, ([country, values]) => {
-    return { 
-      country,
-      WHO_Region: values[0].WHO_Region,
-      averageDiastolic: d3.mean(values, d => d.Diastolic_BP_mmHg),
-      averageSystolic: d3.mean(values, d => d.Systolic_BP_mmHg),
-      sampleSize: values.length
-    }
-  })
-
-  const whoRegions = Array.from(new Set(data.map(d => d.WHO_Region))).sort()
-
-  const diastolicScale = d3.scaleLinear()
-    .domain([70, 80])
-    .range([500, 100])
-
-  const systolicScale = d3.scaleLinear()
-    .domain([120, 135])
-    .range([100, 700])
-
-
-  const sizeScale = d3.scaleLinear()
-    .domain([70, 125])
-    .range([1, 10])
-
-  const whoRegionScale = d3.scaleOrdinal()
-    .domain(whoRegions)
-    .range(d3.schemeAccent)
-
-  d3.select("#container").selectAll("*").remove()
-  const svg = d3.select("#container").append("svg")
-    .attr("viewBox", "0 0 800 600")
-
-  svg.append("g")
-    .attr("id", "horizontal-axis")
-    .attr("transform", "translate(0, 500)")
-    .call(d3.axisBottom(systolicScale))
-    .attr("stroke-width", 2)
-    .selectAll("text")
-      .attr("y", 10)
-
-  svg.append("text")
-  .attr("x", 590)
-  .attr("y", 535)
-  .attr("font-size", 16)
-  .text("Systolic Pressure")
-
- svg.append("g")
-  .attr("id", "vertical-axis")
-  .attr("transform", "translate(100, 0)")
-  .call(d3.axisLeft(diastolicScale))
-  .attr("stroke-width", 2)
-  .selectAll("text")
-  .attr("x", -10)
-
-  svg.append("text")
-    .attr("x", 40)
-    .attr("y", 85)
-    .attr("font-size", 16)
-    .text("Diastolic Pressure")
-
-  svg.selectAll("circle")
-    .data(bloodPressureByCountry)
-    .join("circle")
-    .attr("cx", d => systolicScale(d.averageSystolic))
-    .attr("cy", d => diastolicScale(d.averageDiastolic))
-    .attr("r", d => sizeScale(d.sampleSize))
-    .attr("fill", d => whoRegionScale(d.WHO_Region))
-    .attr("stroke", "black")
-    .attr("stroke-width", 2)
-
-  const legend = svg.append("g")
-    .attr("id", "legend")
-    .attr("transform", "translate(600, 100)")
-  
-    legend
-      .selectAll("rect")
-      .data(whoRegions)
-      .join("rect")
-      .attr("x", 0)
-      .attr("y", (_, i) => i * 30)
-      .attr("width", 20)
-      .attr("height", 20)
-      .attr("fill", d => whoRegionScale(d))
-      .attr("stroke", "black")
-      .attr("stroke-width", 2)
-
-    legend
-      .selectAll("text")
-      .data(whoRegions)
-      .join("text")
-      .attr("x", 30)
-      .attr("y", (_, i) => i * 30 + 15)
-      .attr("font-size", 16)
-      .text(d => d)
-}
-
-function visualizeAgeGroupByRegion() {
-  const ageGroups = [
-    "Infant (0-1)",
-    "Early Childhood (1-5)",
-    "Middle Childhood (6-10)",
-    "Early Adolescence (11-15)",
-    "Late Adolescence (16-18)",
-    "Young Adult (19-29)",
-    "Adult (30-39)",
-    "Middle-Aged (40-49)",
-    "Middle-Aged Senior (50-59)",
-    "Young Elderly (60-69)",
-    "Elderly (70-79)",
-    "Very Elderly (80+)"
+const ageGroups = [
+  "Infant (0-1)",
+  "Early Childhood (1-5)",
+  "Middle Childhood (6-10)",
+  "Early Adolescence (11-15)",
+  "Late Adolescence (16-18)",
+  "Young Adult (19-29)",
+  "Adult (30-39)",
+  "Middle-Aged (40-49)",
+  "Middle-Aged Senior (50-59)",
+  "Young Elderly (60-69)",
+  "Elderly (70-79)",
+  "Very Elderly (80+)"
 ]
 
+function visualizeAgeGroupByRegion() {
   const groupedData = d3.rollup(data, d => d.length, d => d.WHO_Region, d => d.Age_Group)
 
   console.log(groupedData)
@@ -210,26 +51,15 @@ function visualizeAgeGroupByRegion() {
 
   console.log(regions.map(region => groupedData.get(region)))
 
-  const flatData = regions.flatMap(region => groupedData.get(region).entries().map(([ageGroup, count]) => ({ region, ageGroup, count })).toArray())
-  console.log(flatData)
-
-  const indexedData = d3.index(flatData, d => d.region, d => d.ageGroup)
-
-  console.log(indexedData)
-
-  const series = d3.stack()
+  const stacking = d3.stack()
     .keys(ageGroups)
     .value(([, regionalData], ageGroup) => regionalData.get(ageGroup))
 
-  console.log(series)
-  console.log(series(groupedData))
-  console.log(series(groupedData)[0])
+  console.log(stacking(groupedData))
+
+  const stacks = stacking(groupedData)
 
   const regionSizes = regions.map(region => d3.sum(groupedData.get(region).values()))
-
-  console.log(regionSizes)
-
-  console.log(d3.max(regionSizes))
 
   const sizeScale = d3.scaleLinear()
     .domain([0, d3.max(regionSizes)])
@@ -245,10 +75,9 @@ function visualizeAgeGroupByRegion() {
 
   const ageGroupColorScale = d3.scaleOrdinal()
     .domain(ageGroups)
-    .range(d3.schemePaired)
+    .range(ageGroups.map((_, i) => d3.interpolateViridis((ageGroups.length - i - 1) / ageGroups.length)))
 
-
-  const width = 800
+  const width = 1000
   const height = 600
   const margins = {top: 50, right: 50, bottom: 50, left: 50}
   const visibleWidth = width - margins.left - margins.right
@@ -267,17 +96,10 @@ function visualizeAgeGroupByRegion() {
   visibleArea.append("g")
     .attr("id", "horizontal-axis")
     .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(sizeScale))
+    .call(d3.axisBottom(regionScale))
     .attr("stroke-width", 2)
     .selectAll("text")
-      .attr("y", 10)
-      .attr("transform", "rotate(90)")
-
-  visibleArea.append("text")
-  .attr("x", 590)
-  .attr("y", 535)
-  .attr("font-size", 20)
-  .text("WHO region")
+      .attr("font-size", 16)      
 
  visibleArea.append("g")
   .attr("id", "vertical-axis")
@@ -285,15 +107,16 @@ function visualizeAgeGroupByRegion() {
   .attr("stroke-width", 2)
   .selectAll("text")
   .attr("x", -10)
+  .attr("font-size", 16)
 
   visibleArea.append("text")
-    .attr("x", 80)
-    .attr("y", 85)
-    .attr("font-size", 16)
-    .text("#People")
+    .attr("x", 20)
+    .attr("y", 0)
+    .attr("font-size", 20)
+    .text("People")
 
-  const ageGroupVis = visibleArea.selectAll("g.ageGroupRects")
-    .data(series)
+  const ageGroupVis = visibleArea.selectAll(".ageGroupRects")
+    .data(stacks)
     .join("g")
     .attr("class", "ageGroupRects")
     .attr("fill", d => ageGroupColorScale(d.key))
@@ -302,106 +125,45 @@ function visualizeAgeGroupByRegion() {
     .data(d => d)
     .join("rect")
     .attr("x", d => regionScale(d.data[0]))
-    .attr("width", d => regionScale(d.data[0]).bandwidth)
-    .attr("y", d => sizeScale(d[0]))
-    .attr("height", d => sizeScale(d[1]) - sizeScale(d[0]))
+    .attr("width", d => regionScale.bandwidth())
+    .attr("y", d => sizeScale(d[1]))
+    .attr("height", d => sizeScale(d[0]) - sizeScale(d[1]))
+
+  
+  const legend = visibleArea.append("g")
+    .attr("id", "legend")
+    .attr("transform", "translate(900, 0)")
+  
+  legend
+    .selectAll("rect")
+    .data(ageGroups)
+    .join("rect")
+    .attr("x", 0)
+    .attr("y", (_, i) => i * 30)
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("fill", d => ageGroupColorScale(d))
+    .attr("stroke", "black")
+    .attr("stroke-width", 2)
+
+  legend
+    .selectAll("text")
+    .data(ageGroups)
+    .join("text")
+    .attr("x", 30)
+    .attr("y", (_, i) => i * 30 + 15)
+    .attr("font-size", 16)
+    .text(d => d)
 }
 
-function visualizeBloodPressureByCountryAndGender() {
-  const groupedData = d3.group(data, d => d.Country, d => d.Sex)
-  const countries = Array.from(groupedData.keys()).sort()
-  const gender = ["Female", "Male"]
-  const pressures = countries.flatMap(country => gender.map(g => d3.mean(groupedData.get(country).get(g), d => d.Mean_Arterial_Pressure)))
-  console.log(d3.min(pressures))
-  console.log(d3.max(pressures))
+function visualizeAgeGroupByYear() {
+    const groupedData = d3.rollup(data, d => d.length, d => d.Year, d => d.Age_Group)
 
-  const mapScale = d3.scaleLinear()
-  .domain([80, 100])
-  .range([0, 550])
-
-  const countryScale = d3.scaleBand()
-    .domain(countries)
-    .range([0, 4000])
-    .padding(0.2)
-
-  const genderScale = d3.scaleBand()
-    .domain(gender)
-    .range([0, countryScale.bandwidth()])
-    .padding(0.1)
-    
-  const genderColorScale = d3.scaleOrdinal()
-    .domain(gender)
-    .range(d3.schemeCategory10)
-
-  d3.select("#container").selectAll("*").remove()
-
-  const axisSVG = d3.select("#container").append("svg")
-    .attr("viewBox", "0 0 800 70")
-
-  const svg = d3.select("#container")
-    .append("div")
-    .attr("id", "barChartSVG")
-    .append("svg")
-    .attr("viewBox", "0 0 800 4000")
-
-  axisSVG.append("g")
-    .attr("id", "horizontal-axis")
-    .attr("transform", "translate(150, 50)")
-    .call(d3.axisTop(mapScale))
-    .attr("stroke-width", 2)
-    .selectAll("text")
-      .attr("y", 10)
-
-  axisSVG.append("text")
-    .attr("x", 590)
-    .attr("y", 35)
-    .attr("font-size", 16)
-    .text("Mean Arterial Pressure")
-
-  const barChart = svg.append("g")
-    .attr("id", "barChart")
-
-  const countryGroup = barChart.selectAll("g.country-group")
-    .data(countries)
-    .join("g")
-    .attr("class", "country-group")
-    .attr("transform", d => `translate(0, ${countryScale(d)})`)
-
-  countryGroup.selectAll("rect.country-label")
-    .data(d => [d])
-    .join("rect")
-    .attr("class", "country-label")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", 150)
-    .attr("height", countryScale.bandwidth())
-    .attr("fill", "lightgray")
-    .attr("stroke", "black")
-    .attr("stroke-width", 1)
-    .text(d => d)
-
-  countryGroup.selectAll("text.country-label")
-    .data(d => [d])
-    .join("text")
-    .attr("class", "country-label")
-    .attr("x", 10)
-    .attr("y", countryScale.bandwidth() / 2)
-    .attr("dy", "0.35em")
-    .text(d => d)
-
-  countryGroup.selectAll("rect.gender-bar")
-    .data(d => groupedData.get(d).entries())
-    .join ("rect")
-    .attr("class", "gender-bar")
-    .attr("x", 150)
-    .attr("y", d => genderScale(d[0]))
-    .attr("width", d => mapScale(d3.mean(d[1], d => d.Mean_Arterial_Pressure)))
-    .attr("height", genderScale.bandwidth())
-    .attr("fill", d => genderColorScale(d[0]))
+  console.log(groupedData)
 }
 
 async function init() {
   await loadData()
-  visualizeAgeGroupByRegion()
-  document.getElementById("vis1").checked = true
+  visualizeAgeGroupByYear()
+  document.getElementById("vis2").checked = true
 }
